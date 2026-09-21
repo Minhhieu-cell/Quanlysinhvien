09d340d0f7c754724cb72f214d819920cb74b825
Hieu <hieu96014@gmail.com>
2026-09-21 14:29:42 +0700

    Create Student class

---
+++ Quanlysinhvien/src/quanlysinhvien/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanlysinhvien;
+
+/**
+ *
+ * @author P604-M17
+ */
+public class NewClass {
+    
+}
---
+++ Quanlysinhvien/src/quanlysinhvien/Sinhvien1.java
@@ -0,0 +1,13 @@
+
+package quanlysinhvien;
+public class Sinhvien1 {
+    String maSV;
+    String hoTen;
+    double diem;
+    
+    public Sinhvien1(String maSV, String hoTen, double diem){
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.diem = diem;
+    }
+}
